60d8628116735f3058f1446aafb2d55c0054eaa1
Daniel-Rauh <daniel.rauh@gmail.com>
2020-07-08 13:28:07 +0200

    less js and easteregg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
 let x = 0
+function output() {
+    if (x == 69) {
+        document.getElementById("count").innerHTML = "Nice!"
+    }
+    else {
+        document.getElementById("count").innerHTML = x
+    }
+}
 function plusOne() {
     x++
-    document.getElementById("count").innerHTML = x
 }
 function minusOne() {
     x--
-    document.getElementById("count").innerHTML = x
 }
 function plusTen() {
     x=x+10
-    document.getElementById("count").innerHTML = x
 }
 function minusTen() {
     x=x-10
-    document.getElementById("count").innerHTML = x
 }
 function plusHundred() {
     x=x+100
-    document.getElementById("count").innerHTML = x
 }
 function minusHundred() {
     x=x-100
-    document.getElementById("count").innerHTML = x
 }
 function reset() {
     x=0
-    document.getElementById("count").innerHTML = x
 }
 function timesTwo() {
     x=x*2
-    document.getElementById("count").innerHTML = x
 }
